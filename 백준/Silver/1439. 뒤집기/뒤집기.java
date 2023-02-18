941a04bf16cda00044307f9bd5104ef816ac5b85
dongk1m <120110806+dongk1m@users.noreply.github.com>
2023-02-18 15:29:32 +0900

    [Silver V] Title: 뒤집기, Time: 128 ms, Memory: 13292 KB -BaekjoonHub

--- 백준/Silver/1439. 뒤집기/뒤집기.java
+++ 백준/Silver/1439. 뒤집기/뒤집기.java
@@ -1,40 +1,35 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
-
-    public static void main(String[] args) {
-
-        Scanner sc = new Scanner(System.in);
-
-        String str = sc.next();
-
-        int l = str.length();
-
-        char[] sub = new char[l];
-
-        for (int i = 0; i < l; i++) {
-
-            sub[i] = str.charAt(i);
-
-        }
-
-        int cnt = 0;
-
-        for (int i = 0; i < l - 1; i++) {
-
-            if (sub[0] == sub[l - 1]) {
-                if (sub[i] != sub[i + 1]) {
-                    cnt++;
-                }
-            } else if (sub[0] != sub[l - 1]) {
-                if (sub[i] != sub[i + 1]) {
-                    cnt++;
-                }
-            }
-// 101010
-        }
-        if (sub[0] != sub[l - 1]) cnt = cnt+1;
-        System.out.println(cnt / 2);
-    }
-
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		//입력 받기
+		char[] arr = sc.next().toCharArray();
+		
+		//중복 제거하여 01 저장할 arraylist
+		ArrayList<Boolean> chk = new ArrayList<>();
+		
+		//중복 제거하여 저장
+		for (int i = 0; i < arr.length; i++) {
+			if(i==0) {
+				if(arr[i]=='0')chk.add(false);
+				else chk.add(true);
+			}
+			else {
+				if(arr[i]!=arr[i-1]) {
+					if(arr[i]=='0')chk.add(false);
+					else chk.add(true);
+				}
+			}
+		}
+		// 0과 1의 개수를 셀 카운트 배열
+		int[] cnt = new int[2];
+		for (boolean i : chk) {
+			if(i==false)cnt[0]++;
+			else cnt[1]++;
+		}
+		// 0과 1중 작은것이 최소 횟수
+		System.out.println(Math.min(cnt[0], cnt[1]));
+	}
 }
