8a7988c20248bf8f996d30f4c0684c27ba92538d
dongk1m <120110806+dongk1m@users.noreply.github.com>
2023-03-05 22:14:24 +0900

    [Silver III] Title: N과 M (5), Time: 404 ms, Memory: 31208 KB -BaekjoonHub

---
+++ 백준/Silver/15654. N과 M （5）/N과 M （5）.java
@@ -0,0 +1,44 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+	static int N, M;
+	static int[] series, numbers;
+	static boolean[] check;
+	static StringBuilder sb = new StringBuilder();
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		N = sc.nextInt();
+		M = sc.nextInt();
+		series = new int[M + 1];
+		numbers = new int[N + 1];
+		check = new boolean[N + 1];
+		for (int i = 1; i <= N; i++) {
+			numbers[i] = sc.nextInt();
+		}
+		Arrays.sort(numbers);
+		NM(1);
+		System.out.println(sb.toString());
+	}
+
+	private static void NM(int d) {
+		// 깊이의 끝에 도달
+		if (d == M + 1) {
+			for (int i = 1; i <= M; i++) {
+				sb.append(series[i]).append(' ');
+			}
+			sb.append('\n');
+		}
+		// 경우의 수 체크해주고 > 다음 깊이로
+		else {
+			for (int i = 1; i <= N; i++) {
+				if(check[i]) continue;
+				series[d] = numbers[i];
+				check[i] = true;
+				NM(d+1);
+				check[i] = false;
+			}
+		}
+	}
+}
