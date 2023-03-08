67b4df2def16165c371dc223b4c30eeca880c3a5
dongk1m <120110806+dongk1m@users.noreply.github.com>
2023-03-08 19:49:23 +0900

    [Silver V] Title: 너의 평점은, Time: 236 ms, Memory: 18204 KB -BaekjoonHub

---
+++ 백준/Silver/25206. 너의 평점은/너의 평점은.java
@@ -0,0 +1,31 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		Map<String, Double> map = new HashMap<>();
+		map.put("A+", 4.5);
+		map.put("A0", 4.0);
+		map.put("B+", 3.5);
+		map.put("B0", 3.0);
+		map.put("C+", 2.5);
+		map.put("C0", 2.0);
+		map.put("D+", 1.5);
+		map.put("D0", 1.0);
+		map.put("F", 0.0);
+		map.put("P", 0.0);
+		double sum = 0, sum2 = 0;
+		for (int i = 0; i < 20; i++) {
+			sc.next();
+			double num = sc.nextDouble();
+			String s = sc.next();
+			if (!s.equals("P")) {
+				sum2 += num;
+				sum += num * map.get(s);
+			}
+		}
+		System.out.printf("%.6f",sum/sum2);
+	}
+}
