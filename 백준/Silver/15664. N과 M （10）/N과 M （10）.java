cff8eed8c9aa6bff555959e0659b203cbfd8d636
dongk1m <120110806+dongk1m@users.noreply.github.com>
2023-03-23 15:37:55 +0900

    [Silver II] Title: N과 M (10), Time: 216 ms, Memory: 17760 KB -BaekjoonHub

---
+++ 백준/Silver/15664. N과 M （10）/N과 M （10）.java
@@ -0,0 +1,38 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+	static int N, M;
+	static int[] nums, select;
+	static StringBuilder sb;
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		N = sc.nextInt(); M = sc.nextInt();
+		nums = new int[N+1]; select = new int[N+1];
+		sb = new StringBuilder();
+		for (int i = 1; i <= N; i++) {
+			nums[i] = sc.nextInt();
+		}
+		Arrays.sort(nums);
+		NM(1, 1);
+		System.out.println(sb.toString());
+	}
+	private static void NM(int d, int start) {
+		if(d == M+1) {
+			for (int i = 1; i <= M; i++) {
+				sb.append(select[i]).append(' ');
+			}
+			sb.append('\n');
+		}
+		else {
+			int last=0;
+			for (int i = start; i <= N; i++) {
+				if(last == nums[i])continue;
+				select[d] = nums[i];
+				last = nums[i];
+				NM(d+1, i+1);
+			}
+		}
+	}
+	
+}
