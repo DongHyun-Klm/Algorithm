86e0d8927a613b9c7c792510f4af372dfc51ca49
dongk1m <120110806+dongk1m@users.noreply.github.com>
2023-03-29 09:12:57 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 1720 ms, Memory: 329488 KB -BaekjoonHub

--- 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.java
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.java
@@ -1,4 +1,6 @@
 import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Scanner;
 
 public class Main {
@@ -23,17 +25,24 @@ public class Main {
 		}
 		for (int i = 1; i < arr.length; i++) {
 			if (!visited[i]) {
-				DFS(i);
+				BFS(i);
 				cnt++;
 			}
 		}
 		System.out.println(cnt);
 	}
 
-	private static void DFS(int v) {
+	private static void BFS(int v) {
+		Queue<Integer> queue = new LinkedList<>();
+		queue.add(v);
 		visited[v] = true;
-		for (int j = 0; j < arr[v].size(); j++) {
-			if(!visited[arr[v].get(j)]) DFS(arr[v].get(j));
+		while (!queue.isEmpty()) {
+			int x = queue.poll();
+			for (int i = 0; i < arr[x].size(); i++) {
+				if(visited[arr[x].get(i)]) continue;
+				queue.add(arr[x].get(i));
+				visited[arr[x].get(i)] = true;
+			}
 		}
 	}
 }
