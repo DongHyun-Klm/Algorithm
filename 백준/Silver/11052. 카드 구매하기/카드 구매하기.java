cefeca704b78e5194553074e388f99fc7ded48a0
dongk1m <120110806+dongk1m@users.noreply.github.com>
2023-05-09 22:49:31 +0900

    [Silver I] Title: 카드 구매하기, Time: 148 ms, Memory: 14544 KB -BaekjoonHub

--- 백준/Silver/11052. 카드 구매하기/카드 구매하기.java
+++ 백준/Silver/11052. 카드 구매하기/카드 구매하기.java
@@ -1,23 +1,23 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int N = sc.nextInt();
-		int[] card = new int[N];
-		for (int i = 0; i < card.length; i++) {
-			card[i] = sc.nextInt();
-		}
-		// dp[i]는 i개의 카드를 갖기 위해 지불해야 하는 금액의 최대값 
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int[] card = new int[N+1];
 		int[] dp = new int[1001];
-		dp[1] = card[0];
+		for(int i=1;i<=N;i++)card[i]=Integer.parseInt(st.nextToken());
+		dp[1] = card[1];
 		for (int i = 2; i <= N; i++) {
 			for (int j = 1; j < i; j++) {
-				if(i%j==0) {
-					dp[i] = Math.max(dp[j]*(i/j), card[i-1]);
-				}
-				dp[i] = Math.max(dp[i], dp[i-j]+dp[j]);
+				if(i%j==0) dp[i] = Math.max(dp[j]*(i/j), dp[i]);
+				dp[i] = Math.max(dp[i], dp[j]+dp[i-j]);
 			}
+			dp[i] = Math.max(dp[i], card[i]);
 		}
 		System.out.println(dp[N]);
 	}
