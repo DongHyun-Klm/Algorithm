dc1a40106c58325b87e5fa4298ce7f87d83badf8
dongk1m <120110806+dongk1m@users.noreply.github.com>
2023-06-06 21:02:22 +0900

    [Silver II] Title: 마인크래프트, Time: 680 ms, Memory: 35896 KB -BaekjoonHub

--- 백준/Silver/18111. 마인크래프트/마인크래프트.java
+++ 백준/Silver/18111. 마인크래프트/마인크래프트.java
@@ -1,54 +1,42 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Scanner;
 import java.util.StringTokenizer;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken()), M = Integer.parseInt(st.nextToken()), B = Integer.parseInt(st.nextToken());
+        int[][] arr = new int[N][M];
+        int total = B, min = 257, max = -1;
+        for(int i=0;i<N;i++){
+            st = new StringTokenizer(br.readLine());
+            for(int j=0;j<M;j++) {
+                arr[i][j] = Integer.parseInt(st.nextToken());
+                total += arr[i][j];
+                min = Math.min(min, arr[i][j]);
+                max = Math.max(max, arr[i][j]);
+            }
+        }
+        total /= N*M;
+        if(total>max) total = max;
 
-		int N = Integer.parseInt(st.nextToken()), M = Integer.parseInt(st.nextToken()),
-				B = Integer.parseInt(st.nextToken());
-		int maxHeight, leastTime = Integer.MAX_VALUE, total=B,max=Integer.MIN_VALUE,min=Integer.MAX_VALUE;
-		int[] arr = new int[257];
-		int height=-1;
-		for (int i = 0; i < N; i++) {
-			st = new StringTokenizer(br.readLine());
-			for (int j = 0; j < M; j++) {
-				arr[Integer.parseInt(st.nextToken())] += 1;
-			}
-		}
-		
-		for (int i = 0; i < arr.length; i++) {
-			total += arr[i]*i;
-			if(arr[i]>0) {
-				max = Math.max(max, i);
-				min = Math.min(min, i);
-			}
-		}
-		maxHeight = total/(N*M);
-		if(maxHeight>max)maxHeight=max;
-
-		
-		while(maxHeight>=min) {
-			int time = 0;
-			for (int i = 0; i < maxHeight; i++) {
-				time += (maxHeight-i)*arr[i];
-			}
-			for (int i = maxHeight+1; i<257; i++) {
-				time += (i-maxHeight)*arr[i]*2;
-			}
-			
-			if(leastTime>time) {
-				leastTime = time;
-				height = maxHeight;
-			}
-			maxHeight--;
-		}
-		
-		System.out.printf("%d %d",leastTime,height);
-	}
-
+        int answer = Integer.MAX_VALUE;
+        int height = min, max_height = -1;
+        while (height<=total){
+            int temp = 0;
+            for(int i=0;i<N;i++){
+                for(int j=0;j<M;j++) {
+                    if(arr[i][j]>height) temp += 2*(arr[i][j] - height);
+                    else temp += height - arr[i][j];
+                }
+            }
+            answer = Math.min(answer, temp);
+            if(answer==temp) max_height = height;
+            height++;
+        }
+        System.out.println(answer + " " + max_height);
+    }
 }
-
