1ec13b4d228d4d9d49bf4dedf5aafcdcd904d7da
dongk1m <120110806+dongk1m@users.noreply.github.com>
2023-06-18 16:31:13 +0900

    [Silver I] Title: 트리 순회, Time: 216 ms, Memory: 17636 KB -BaekjoonHub

--- 백준/Silver/1991. 트리 순회/트리 순회.java
+++ 백준/Silver/1991. 트리 순회/트리 순회.java
@@ -1,56 +1,54 @@
-import java.util.ArrayList;
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
-	static StringBuilder sb = new StringBuilder();
-	static ArrayList<node>[] arr;
-	static class node{
-		int left, right;
-		node(int left, int right){
-			this.left = left;
-			this.right = right;
-		}
-	}
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int N = sc.nextInt();
-		arr = new ArrayList[N+1];
-		for (int i = 1; i < arr.length; i++) 
-			arr[i] = new ArrayList<>();
-		for (int i = 1; i < arr.length; i++) {
-			int rot = sc.next().charAt(0)-64;
-			int lef = sc.next().charAt(0)-64;
-			int rig = sc.next().charAt(0)-64;
-			arr[rot].add(new node(lef, rig));
-		}
-		in_order(1);
-		sb.append('\n');
-		pre_order(1);
-		sb.append('\n');
-		post_order(1);
-		System.out.println(sb.toString());
-	}
-	private static void in_order(int x) {
-		sb.append((char)(x+64));
-		if(arr[x].get(0).left>0)
-			in_order(arr[x].get(0).left);
-		if(arr[x].get(0).right>0)
-			in_order(arr[x].get(0).right);
-	}
-	
-	private static void pre_order(int x) {
-		if(arr[x].get(0).left>0)
-			pre_order(arr[x].get(0).left);
-		sb.append((char)(x+64));
-		if(arr[x].get(0).right>0)
-			pre_order(arr[x].get(0).right);
-	}
-	
-	private static void post_order(int x) {
-		if(arr[x].get(0).left>0)
-			post_order(arr[x].get(0).left);
-		if(arr[x].get(0).right>0)
-			post_order(arr[x].get(0).right);
-		sb.append((char)(x+64));
-	}
+    static class node{
+        int left, right;
+        node (int l, int r){
+            this.left = l;
+            this.right = r;
+        }
+    }
+    static ArrayList<node>[] arr = new ArrayList[26];;
+    static StringBuilder sb = new StringBuilder();
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        for(int i=0; i<26; i++){
+            arr[i] = new ArrayList<>();
+        }
+        for(int i=1; i<=N; i++){
+            int now = sc.next().charAt(0)-'A';
+            int left = sc.next().charAt(0)-'A';
+            int right = sc.next().charAt(0)-'A';
+            arr[now].add(new node(left, right));
+        }
+
+        preorder(0); sb.append('\n');
+        inorder(0); sb.append('\n');
+        postorder(0);
+        System.out.println(sb);
+    }
+    //전위 순회
+    static void preorder(int v){
+        node now = arr[v].get(0);
+        sb.append((char)(v+'A'));
+        if(now.left>=0) preorder(now.left);
+        if(now.right>=0) preorder(now.right);
+    }
+
+    //중위 순회
+    static void inorder(int v){
+        node now = arr[v].get(0);
+        if(now.left>=0) inorder(now.left);
+        sb.append((char)(v+'A'));
+        if(now.right>=0) inorder(now.right);
+    }
+
+    //후위 순회
+    static void postorder(int v){
+        node now = arr[v].get(0);
+        if(now.left>=0) postorder(now.left);
+        if(now.right>=0) postorder(now.right);
+        sb.append((char)(v+'A'));
+    }
 }
