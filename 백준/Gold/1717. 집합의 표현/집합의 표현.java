dacb233f9006eaa722085226c8641038e16a5b22
dongk1m <120110806+dongk1m@users.noreply.github.com>
2023-06-23 16:54:25 +0900

    [Gold V] Title: 집합의 표현, Time: 492 ms, Memory: 55756 KB -BaekjoonHub

--- 백준/Gold/1717. 집합의 표현/집합의 표현.java
+++ 백준/Gold/1717. 집합의 표현/집합의 표현.java
@@ -5,37 +5,55 @@ import java.util.*;
 
 public class Main {
     static int[] p;
+    static int[] rank;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         StringBuilder sb = new StringBuilder();
         int N = Integer.parseInt(st.nextToken()), M = Integer.parseInt(st.nextToken());
         p = new int[N+1];
-        for(int i=0;i<=N;i++) p[i] = i;
-        for(int i=0;i<M;i++){
+        rank = new int[N+1];
+        for(int i=0; i<=N; i++) {
+            p[i] = i;
+            rank[i] = 0;
+        }
+        for(int i=0; i<M; i++){
             st = new StringTokenizer(br.readLine());
             int option = Integer.parseInt(st.nextToken());
-            int a = Integer.parseInt(st.nextToken()), b =Integer.parseInt(st.nextToken());
-            if(option==0){
+            int a = Integer.parseInt(st.nextToken()), b = Integer.parseInt(st.nextToken());
+            if(option == 0){
                 union(a, b);
-            }
-            else{
-                if(findSet(a)==findSet(b)) sb.append("YES").append('\n');
-                else sb.append("NO").append('\n');
+            } else {
+                if(findSet(a) == findSet(b)) {
+                    sb.append("YES").append('\n');
+                } else {
+                    sb.append("NO").append('\n');
+                }
             }
         }
         System.out.print(sb);
     }
 
     private static int findSet(int a) {
-        if(p[a] != a) p[a] = findSet(p[a]);
+        if(p[a] != a) {
+            p[a] = findSet(p[a]);
+        }
         return p[a];
     }
 
     private static void union(int a, int b) {
-        a = findSet(a);
-        b = findSet(b);
-        if(a<b) p[b] = a;
-        else p[a] = b;
+        int rootA = findSet(a);
+        int rootB = findSet(b);
+        if(rootA != rootB) {
+            if(rank[rootA] < rank[rootB]) {
+                p[rootA] = rootB;
+            } else if(rank[rootA] > rank[rootB]) {
+                p[rootB] = rootA;
+            } else {
+                p[rootA] = rootB;
+                rank[rootB]++;
+            }
+        }
     }
 }
