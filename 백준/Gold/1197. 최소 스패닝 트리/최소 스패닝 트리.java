b534e9251bcd4290453ff54977f1668fdf59c458
dongk1m <120110806+dongk1m@users.noreply.github.com>
2023-06-23 17:27:01 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 1400 ms, Memory: 250656 KB -BaekjoonHub

--- 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.java
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.java
@@ -4,49 +4,56 @@ import java.util.Scanner;
 
 public class Main {
 	static int[] p;
+	static int[] rank;
+
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		int V = sc.nextInt();
-		int E = sc.nextInt();
-		p = new int[V+1];
+		int V = sc.nextInt(), E = sc.nextInt();
+		p = new int[V + 1];
+		rank = new int[V+1];
+		for (int i = 1; i < p.length; i++) {
+			p[i] = i;
+		}
 		int[][] arr = new int[E][3];
 		for (int i = 0; i < E; i++) {
-			arr[i][0] = sc.nextInt();
-			arr[i][1] = sc.nextInt();
-			arr[i][2] = sc.nextInt();
+			for (int j = 0; j < 3; j++) {
+				arr[i][j] = sc.nextInt();
+			}
 		}
-		Arrays.sort(arr,new Comparator<int[]>() {
+		Arrays.sort(arr, new Comparator<int[]>() {
 			@Override
 			public int compare(int[] o1, int[] o2) {
 				return o1[2] - o2[2];
 			}
 		});
-		for (int i = 1; i <= V; i++) {
-			p[i] = i;
-		}
 		int ans = 0;
 		int pick = 0;
-		int i = 0;
-		while (pick!=V-1) {
-			int nx = findSet(arr[i][0]);
-			int ny = findSet(arr[i][1]);
-			if(nx!=ny) {
-				union(nx, ny);
+		for (int i = 0; i < E; i++) {
+			int x = arr[i][0];
+			int y = arr[i][1];
+			if (findSet(x) != findSet(y)) {
 				ans += arr[i][2];
 				pick++;
+				union(x, y);
 			}
-			i++;
+			if (pick == V - 1)
+				break;
 		}
 		System.out.println(ans);
 	}
-	private static int findSet(int i) {
-		if(i == p[i]) return i;
-		p[i] = findSet(p[i]);
-		return p[i];
+
+	private static void union(int x, int y) {
+		if (rank[x] > rank[y])
+			p[findSet(y)] = findSet(x);
+		else {
+			p[findSet(x)] = findSet(y);
+			rank[y]++;
+		}
 	}
-	
-	private static void union(int nx, int ny) {
-		if(nx > ny) p[ny] = nx;
-		else p[nx] = ny;
+
+	private static int findSet(int x) {
+		if (x != p[x])
+			p[x] = findSet(p[x]);
+		return p[x];
 	}
 }
