bac99c697793dea188630fe8c2927185e2fac0b9
dongk1m <120110806+dongk1m@users.noreply.github.com>
2023-06-28 22:30:03 +0900

    [Gold V] Title: 숨바꼭질 3, Time: 268 ms, Memory: 22500 KB -BaekjoonHub

--- 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.java
+++ 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.java
@@ -1,52 +1,56 @@
+
+import java.util.Deque;
 import java.util.LinkedList;
-import java.util.Queue;
 import java.util.Scanner;
 
 public class Main {
-	static int N, K;
-	static class node{
-		int x, dist;
-		node(int x, int dist){
-			this.x = x;
-			this.dist = dist;
-		}
-	}
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		N = sc.nextInt();
-		K = sc.nextInt();
-		BFS();
-	}
-	private static void BFS() {
-		Queue<node> queue = new LinkedList<>();
-		boolean[] visited = new boolean[100001];
-		queue.add(new node(N, 0));
-		visited[N] = true;
-		while (!queue.isEmpty()) {
-			node now = queue.poll();
-			if(now.x == K) {
-				System.out.println(now.dist);
-				return;
-			}
-			// * 2
-			int x = now.x;
-			while (2 * x <= 100000 && x != 0) {
-				x *= 2;
-				if(!visited[x]) {
-					visited[x] = true;
-					queue.add(new node(x, now.dist));
-				}
-			}
-			// + 1
-			if(now.x < 100000 && !visited[now.x+1]) {
-				visited[now.x+1] = true;
-				queue.add(new node(now.x+1, now.dist+1));
-			}
-			// - 1
-			if(now.x > 0 && !visited[now.x-1]) {
-				visited[now.x-1] = true;
-				queue.add(new node(now.x-1, now.dist+1));
-			}
-		}
-	}
+    static int N, K;
+    static class node{
+        int x, dist;
+        node(int x, int dist){
+            this.x = x;
+            this.dist = dist;
+        }
+        public String toString(){
+            return x + " " + dist;
+        }
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        N = sc.nextInt();
+        K = sc.nextInt();
+        BFS();
+    }
+    private static void BFS() {
+        Deque<node> queue = new LinkedList<>();
+        boolean[] visited = new boolean[100001];
+        queue.add(new node(N, 0));
+        visited[N] = true;
+        while (!queue.isEmpty()) {
+            node now = queue.poll();
+            if(now.x == K) {
+                System.out.println(now.dist);
+                return;
+            }
+            // * 2
+            int x = now.x;
+            while (2 * x <= 100000 && x != 0) {
+                x *= 2;
+                if(x == K || !visited[x]) {
+                    visited[x] = true;
+                    queue.addFirst(new node(x, now.dist));
+                }
+            }
+            // + 1
+            if(now.x < 100000 && !visited[now.x+1]) {
+                visited[now.x+1] = true;
+                queue.add(new node(now.x+1, now.dist+1));
+            }
+            // - 1
+            if(now.x > 0 && !visited[now.x-1]) {
+                visited[now.x-1] = true;
+                queue.add(new node(now.x-1, now.dist+1));
+            }
+        }
+    }
 }
