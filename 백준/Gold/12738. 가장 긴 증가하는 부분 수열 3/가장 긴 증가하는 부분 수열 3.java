a8b138e8df2c3ec229b99d2b4576ce66d8dec185
DongHyun-Klm <120110806+DongHyun-Klm@users.noreply.github.com>
2023-09-19 00:56:18 +0900

    [Gold II] Title: 가장 긴 증가하는 부분 수열 3, Time: 2064 ms, Memory: 331224 KB -BaekjoonHub

---
+++ 백준/Gold/12738. 가장 긴 증가하는 부분 수열 3/가장 긴 증가하는 부분 수열 3.java
@@ -0,0 +1,29 @@
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        List<Integer> list = new ArrayList<>();
+        list.add(Integer.MIN_VALUE);
+        for (int i = 0; i < N; i++) {
+            int now = sc.nextInt();
+            if(now > list.get(list.size()-1)) list.add(now);
+            else {
+                int start = 0;
+                int end = list.size() - 1;
+                while (start < end) {
+                    int mid = (start + end) / 2;
+                    if(list.get(mid) < now) start = mid + 1;
+                    else end = mid;
+                }
+                list.set(end, now);
+            }
+        }
+        System.out.println(list.size()-1);
+    }
+}
