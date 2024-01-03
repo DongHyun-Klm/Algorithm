880dea58c8d352a4e1341021fa76afe6c88b2776
DongHyun-Klm <120110806+DongHyun-Klm@users.noreply.github.com>
2024-01-03 15:52:33 +0900

    [Gold IV] Title: 스카이라인 쉬운거, Time: 236 ms, Memory: 20260 KB -BaekjoonHub

---
+++ 백준/Gold/1863. 스카이라인 쉬운거/스카이라인 쉬운거.java
@@ -0,0 +1,49 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        StringTokenizer st;
+        int[][] altitude = new int[n][2];
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            altitude[i][0] = Integer.parseInt(st.nextToken());
+            altitude[i][1] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(altitude, (a,b) -> b[0] - a[0]);
+
+        int answer = 0;
+        PriorityQueue<Integer> pq = new PriorityQueue<>((a,b) -> b - a);
+        for (int i = 0; i < n ; i++) {
+            if(altitude[i][1] == 0){
+                pq.clear();
+                continue;
+            }
+            if(pq.isEmpty()) {
+                pq.add(altitude[i][1]);
+                answer++;
+                continue;
+            }
+            if(altitude[i][1] >= pq.peek()) {
+                pq.add(altitude[i][1]);
+                answer++;
+            }
+            else {
+                while (!pq.isEmpty() && pq.peek() > altitude[i][1]) pq.poll();
+                if(!pq.isEmpty() && pq.peek() == altitude[i][1]) {
+                    continue;
+                }
+                pq.add(altitude[i][1]);
+                answer++;
+            }
+        }
+        System.out.println(answer);
+    }
+}
