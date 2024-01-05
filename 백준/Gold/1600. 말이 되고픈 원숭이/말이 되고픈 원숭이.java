95d2c556361c61eea96444e5afbac51060d9eb4f
DongHyun-Klm <120110806+DongHyun-Klm@users.noreply.github.com>
2024-01-05 18:52:17 +0900

    [Gold III] Title: 말이 되고픈 원숭이, Time: 648 ms, Memory: 97164 KB -BaekjoonHub

--- 백준/Gold/1600. 말이 되고픈 원숭이/말이 되고픈 원숭이.java
+++ 백준/Gold/1600. 말이 되고픈 원숭이/말이 되고픈 원숭이.java
@@ -1,87 +1,117 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.sql.SQLOutput;
+import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.Queue;
-import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class Main {
-	static int[][] dih = { { -2, 1 }, { -1, 2 }, { 1, 2 }, { 2, 1 }, { 2, -1 }, { 1, -2 }, { -1, -2 }, { -2, -1 } };
-	static int[][] dir = { { 1, 0 }, { 0, 1 }, { -1, 0 }, { 0, -1 } };
-	static boolean[][][] vistied;
-	static int K, W, H;
-	static int[][] arr;
 
-	static class node {
-		int r;
-		int c;
-		int horse;
-		int dist;
+    static int[] dr = new int[] {-1,1,0,0}; //원숭이의 움직임
+    static int[] dc = new int[] {0,0, -1,1}; //원숭이의 움직임
 
-		node(int r, int c, int horse, int dist) {
-			this.r = r;
-			this.c = c;
-			this.horse = horse;
-			this.dist = dist;
-		}
-	}
+    static int[] hDr = new int[] {-2, -1, -2, -1, 1, 2, 1, 2}; //말 행 움직임
+    static int[] hDc = new int[] {-1, -2, 1, 2, -2, -1, 2, 1}; //말 열 움직임
 
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		K = sc.nextInt();
-		W = sc.nextInt();
-		H = sc.nextInt();
-		arr = new int[H][W];
-		vistied = new boolean[H][W][K+1];
-		for (int i = 0; i < H; i++) {
-			for (int j = 0; j < W; j++) {
-				arr[i][j] = sc.nextInt();
-			}
-		}
-		BFS();
-	}
+    static int K;
+    static int W;
+    static int H;
+    static int min;
 
-	private static void BFS() {
-		Queue<node> queue = new LinkedList<>();
-		queue.add(new node(0, 0, 0, 0));
-		vistied[0][0][0] = true;
-		while (!queue.isEmpty()) {
-			node now = queue.poll();
-			if(now.r == H-1 && now.c == W-1) {
-				System.out.println(now.dist);
-				return;
-			}
-			// 말 기회 다 썻으면
-			if (now.horse == K) {
-				// 원숭이처럼 움직이자
-				for (int i = 0; i < 4; i++) {
-					int nr = now.r + dir[i][0];
-					int nc = now.c + dir[i][1];
-					if (nr >= 0 && nc >= 0 && nr < H && nc < W && !vistied[nr][nc][now.horse] && arr[nr][nc] == 0) {
-						vistied[nr][nc][now.horse] = true;
-						queue.add(new node(nr, nc, now.horse, now.dist + 1));
-					}
-				}
-			}
-			// 기회 남았으면
-			else {
-				// 말처럼 움직이자
-				for (int i = 0; i < 8; i++) {
-					int nr = now.r + dih[i][0];
-					int nc = now.c + dih[i][1];
-					if (nr >= 0 && nc >= 0 && nr < H && nc < W && !vistied[nr][nc][now.horse+1] && arr[nr][nc] == 0) {
-						vistied[nr][nc][now.horse+1] = true;
-						queue.add(new node(nr, nc, now.horse + 1, now.dist + 1));
-					}
-				}
-				// 원숭이처럼 움직이자
-				for (int i = 0; i < 4; i++) {
-					int nr = now.r + dir[i][0];
-					int nc = now.c + dir[i][1];
-					if (nr >= 0 && nc >= 0 && nr < H && nc < W && !vistied[nr][nc][now.horse] && arr[nr][nc] == 0) {
-						vistied[nr][nc][now.horse] = true;
-						queue.add(new node(nr, nc, now.horse, now.dist + 1));
-					}
-				}
-			}
-		}
-		System.out.println(-1);
-	}
+    static int[][] map;
+    static int[][][] visited;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        K = Integer.parseInt(br.readLine());
+        st = new StringTokenizer(br.readLine());
+
+        W = Integer.parseInt(st.nextToken()); //가로길이(열)
+        H = Integer.parseInt(st.nextToken()); //세로길이(행)
+
+        map = new int[H][W];
+        visited = new int[K+2][H][W];
+
+
+
+        min = Integer.MAX_VALUE;
+        for(int h=0; h<H; h++){
+            st = new StringTokenizer(br.readLine());
+            for(int w=0; w<W; w++){
+                map[h][w] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        bfs(0,0);
+
+        if(min==Integer.MAX_VALUE){
+            System.out.println(-1);
+        }else{
+            System.out.println(min);
+        }
+
+    }
+
+
+    static void bfs(int r, int c){
+        Queue<int[]> queue = new LinkedList<>(); //0 : 행 / 1 : 열 / 2 : rank  / 3 : 그 당시 K
+
+        queue.add(new int[] {r, c, 0, 0});
+        visited[0][r][c] =1;
+        visited[1][r][c] = K;
+
+        while(!queue.isEmpty()){
+            int[] now = queue.poll();
+
+            if(now[0]==H-1 && now[1]==W-1){
+                min = Math.min(now[2], min);
+                return;
+            }
+
+
+            //원숭이 이동
+            for(int i=0; i<4; i++){
+                int du = now[0] + dr[i];
+                int dv = now[1] + dc[i];
+
+
+
+
+
+                //map 범위 내인지 체크
+                if(du>=0 && dv>=0 && du<H && dv<W && map[du][dv] ==0 && visited[now[3]][du][dv]==0 ){
+
+
+                    queue.add(new int[] {du, dv, now[2]+1, now[3]});
+                    for(int k=0; k<=K; k++){
+                        visited[now[3]][du][dv] =1;
+                    }
+
+
+                }
+            }
+
+            //말처럼 이동할 횟수가 남았다면
+            if(now[3]<K){
+                for(int i=0; i<8; i++){
+                    int du = now[0] + hDr[i];
+                    int dv = now[1] + hDc[i];
+
+                    if(du>=0 && dv>=0 && du<H && dv<W && map[du][dv]==0 && visited[now[3]+1][du][dv]==0 ){
+                        visited[now[3]+1][du][dv] ++;
+                        queue.add(new int[] {du, dv, now[2]+1, now[3]+1});
+
+
+                    }
+
+                }
+
+            }
+        }
+
+
+    }
 }
