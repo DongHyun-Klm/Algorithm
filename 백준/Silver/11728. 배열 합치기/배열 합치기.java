3a94529d537c35c39f5f39e4600863d8c99f4b27
DongHyun-Klm <120110806+DongHyun-Klm@users.noreply.github.com>
2024-01-14 22:20:11 +0900

    [Silver V] Title: 배열 합치기, Time: 1512 ms, Memory: 314124 KB -BaekjoonHub

--- 백준/Silver/11728. 배열 합치기/배열 합치기.java
+++ 백준/Silver/11728. 배열 합치기/배열 합치기.java
@@ -1,40 +1,87 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.*;
 
 public class Main {
-    static int N, M;
-    static int[] arr1, arr2;
-    static StringTokenizer st;
     static StringBuilder sb = new StringBuilder();
-    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    public static void main(String[] args) throws IOException {
-        input();
-        int point1 = 0, point2 = 0;
-        while (point1 != N || point2 != M)
-        {
-            if(point1 != N && (point2 == M || arr1[point1] < arr2[point2])) {
-                sb.append(arr1[point1++]).append(' ');
-            }
-            else sb.append(arr2[point2++]).append(' ');
+    static FastReader scan = new FastReader();
+
+    static int n, m;
+    static int[] a, b;
+
+    static void input() {
+        n = scan.nextInt();
+        m = scan.nextInt();
+        a = new int[n + 1];
+        b = new int[m + 1];
+        for (int i = 1; i <= n; i++) {
+            a[i] = scan.nextInt();
         }
-        System.out.print(sb);
+        for (int i = 1; i <= m; i++) {
+            b[i] = scan.nextInt();
+        }
     }
 
-    private static void input() throws IOException {
-        st = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(st.nextToken());
-        arr1 = new int[N];
-        M = Integer.parseInt(st.nextToken());
-        arr2 = new int[M];
-        st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < N; i++) {
-            arr1[i] = Integer.parseInt(st.nextToken());
+    static void pro() {
+        int L = 1, R = 1;
+        // a와 b를 앞에서부터 하나씩 추출해서 출력한다. 단, 둘 다 비어있지 않은 경우와 그것이 아닌 경우를 잘 나누자.
+        while (L <= n && R <= m){
+            if (a[L] <= b[R]) sb.append(a[L++]).append(' ');
+            else sb.append(b[R++]).append(' ');
         }
-        st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < M; i++) {
-            arr2[i] = Integer.parseInt(st.nextToken());
+        while (L <= n) sb.append(a[L++]).append(' ');
+        while (R <= m) sb.append(b[R++]).append(' ');
+        
+        System.out.println(sb);
+    }
+
+    public static void main(String[] args) {
+        input();
+        pro();
+    }
+
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        public FastReader(String s) throws FileNotFoundException {
+            br = new BufferedReader(new FileReader(new File(s)));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
         }
     }
 }
