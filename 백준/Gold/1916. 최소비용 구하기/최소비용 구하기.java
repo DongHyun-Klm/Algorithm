2cb662da80d02ad82bf3b840eb88f398cd472cdf
DongHyun-Klm <120110806+DongHyun-Klm@users.noreply.github.com>
2024-01-26 17:09:04 +0900

    [Gold V] Title: 최소비용 구하기, Time: 520 ms, Memory: 51120 KB -BaekjoonHub

--- 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.java
+++ 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.java
@@ -1,59 +1,53 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.PriorityQueue;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-	static class node implements Comparable<node>{
-		int v, dist;
-		node(int v, int dist){
-			this.v = v;
-			this.dist = dist;
-		}
-		@Override
-		public int compareTo(node o) {
-			return dist - o.dist;
-		}
-	}
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
-		int N = Integer.parseInt(br.readLine());
-		int M = Integer.parseInt(br.readLine());
-		ArrayList<node>[] arr = new ArrayList[N+1];
-		for (int i = 1; i < arr.length; i++) arr[i] = new ArrayList<>();
-		for (int i = 0; i < M; i++) {
-			st = new StringTokenizer(br.readLine());
-			int x = Integer.parseInt(st.nextToken());
-			int y = Integer.parseInt(st.nextToken());
-			int w = Integer.parseInt(st.nextToken());
-			arr[x].add(new node(y, w));
-		}
-		st = new StringTokenizer(br.readLine());
-		int start = Integer.parseInt(st.nextToken());
-		int target = Integer.parseInt(st.nextToken());
-		int[] dist = new int[N+1];
-		boolean[] visited = new boolean[N+1];
-		Arrays.fill(dist, Integer.MAX_VALUE);
-		dist[start] = 0;
-		PriorityQueue<node> queue = new PriorityQueue<>();
-		queue.add(new node(start, 0));
-		while (!queue.isEmpty()) {
-			node now = queue.poll();
-			if(visited[now.v]) continue;
-			visited[now.v] = true;
-			for(node n : arr[now.v]) {
-				if(now.dist + n.dist < dist[n.v]) {
-					dist[n.v] = now.dist + n.dist;
-					queue.add(new node(n.v, dist[n.v]));
-				}
-			}
-		}
-		System.out.println(dist[target]);
-	}
+    static int N, M, from, to;
+    static ArrayList<edge>[] al;
+    static StringBuilder sb = new StringBuilder();
+    static StringTokenizer st;
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static class edge {
+        int v, w;
+        public edge(int v, int w) {
+            this.v = v;
+            this.w = w;
+        }
+    }
+    public static void main(String[] args) throws IOException {
+        input();
+        int[] dist = new int[N + 1];
+        Arrays.fill(dist, Integer.MAX_VALUE);
+        dist[from] = 0;
+        PriorityQueue<edge> pq = new PriorityQueue<>((a,b) -> a.w - b.w);
+        pq.add(new edge(from, 0));
+        while (!pq.isEmpty()) {
+            edge now = pq.poll();
+            // 낡은 정보는 버린다
+            if(dist[now.v] != now.w) continue;
+            for(edge e : al[now.v]) {
+                if(dist[e.v] > dist[now.v] + e.w) {
+                    dist[e.v] = dist[now.v] + e.w;
+                    pq.add(new edge(e.v, dist[e.v]));
+                }
+            }
+        }
+        System.out.println(dist[to]);
+    }
+
+    private static void input() throws IOException {
+        N = Integer.parseInt(br.readLine());
+        M = Integer.parseInt(br.readLine());
+        al = new ArrayList[N+1];
+        for (int i = 1; i <= N; i++) al[i] = new ArrayList<>();
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken()), b = Integer.parseInt(st.nextToken());
+            int w = Integer.parseInt(st.nextToken());
+            al[a].add(new edge(b, w));
+        }
+        st = new StringTokenizer(br.readLine());
+        from = Integer.parseInt(st.nextToken());
+        to = Integer.parseInt(st.nextToken());
+    }
 }
