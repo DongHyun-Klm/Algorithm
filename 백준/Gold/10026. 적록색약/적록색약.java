4129a0fda1ef5bef63f9dd18a381d4d1622894c8
DongHyun-Klm <120110806+DongHyun-Klm@users.noreply.github.com>
2024-03-07 17:14:26 +0900

    [Gold V] Title: 적록색약, Time: 160 ms, Memory: 17392 KB -BaekjoonHub

--- 백준/Gold/10026. 적록색약/적록색약.java
+++ 백준/Gold/10026. 적록색약/적록색약.java
@@ -1,64 +1,71 @@
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-	static int N;
-	static char[][] arr;
-	static boolean[][][] visited;
-	static int[] count;
-	static int[][] dir = {{1,0},{0,1},{-1,0},{0,-1}};
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		N = sc.nextInt();
-		arr = new char[N][N];
-		for (int i = 0; i < N; i++) {
-			arr[i] = sc.next().toCharArray();
-		}
-		count = new int[2];
-		visited = new boolean[N][N][2];
-		// 일반 사람
-		for (int i = 0; i < N; i++) {
-			for (int j = 0; j < N; j++) {
-				if(!visited[i][j][0]) {
-					count[0]++;
-					DFS1(i,j);
-				}
-			}
-		}
-		for (int i = 0; i < N; i++) {
-			for (int j = 0; j < N; j++) {
-				if(arr[i][j] == 'R') arr[i][j] = 'G';
-			}
-		}
-		// 색약
-		for (int i = 0; i < N; i++) {
-			for (int j = 0; j < N; j++) {
-				if(!visited[i][j][1]) {
-					count[1]++;
-					DFS2(i,j);
-				}
-			}
-		}
-		System.out.print(count[0] + " " + count[1]);
-	}
-	private static void DFS1(int r, int c) {
-		visited[r][c][0] = true;
-		for (int i = 0; i < 4; i++) {
-			int nr = r + dir[i][0];
-			int nc = c + dir[i][1];
-			if(nr<0||nc<0||nr>=N||nc>=N) continue;
-			if(visited[nr][nc][0]) continue;
-			if(arr[r][c] ==  arr[nr][nc]) DFS1(nr, nc);
-		}
-	}
-	private static void DFS2(int r, int c) {
-		visited[r][c][1] = true;
-		for (int i = 0; i < 4; i++) {
-			int nr = r + dir[i][0];
-			int nc = c + dir[i][1];
-			if(nr<0||nc<0||nr>=N||nc>=N) continue;
-			if(visited[nr][nc][1]) continue;
-			if(arr[r][c] ==  arr[nr][nc]) DFS2(nr, nc);
-		}
-		
-	}
+    static StringTokenizer st;
+    static StringBuilder sb = new StringBuilder();
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static int N;
+    static int[][] dir = {{1,0},{0,1},{-1,0},{0,-1}};
+    static char[][] arr;
+    static boolean[][] visit1, visit2;
+
+    public static void main(String[] args) throws IOException {
+        input();
+        int cnt1 = 0, cnt2 = 0;
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                if(!visit1[i][j]) {
+                    cnt1++;
+                    DFS1(i, j);
+                }
+                if(!visit2[i][j]) {
+                    cnt2++;
+                    DFS2(i, j);
+                }
+            }
+        }
+        System.out.println(cnt1 + " " + cnt2);
+    }
+
+
+    private static void DFS1(int r, int c) {
+        visit1[r][c] = true;
+        for (int k = 0; k < 4; k++) {
+            int nr = r + dir[k][0];
+            int nc = c + dir[k][1];
+            if(nr>=N||nc>=N||nr<0||nc<0) continue;
+            if(visit1[nr][nc]) continue;
+            if(arr[nr][nc] != arr[r][c]) continue;
+            DFS1(nr, nc);
+        }
+    }
+
+    private static void DFS2(int r, int c) {
+        visit2[r][c] = true;
+        for (int k = 0; k < 4; k++) {
+            int nr = r + dir[k][0];
+            int nc = c + dir[k][1];
+            if(nr>=N||nc>=N||nr<0||nc<0) continue;
+            if(visit2[nr][nc]) continue;
+            if(arr[r][c] == 'B') {
+                if(arr[nr][nc] != 'B') continue;
+            }
+            else {
+                if(arr[nr][nc] == 'B') continue;
+            }
+            DFS2(nr, nc);
+        }
+    }
+
+    private static void input() throws IOException {
+        st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        arr = new char[N][N];
+        visit1 = new boolean[N][N];
+        visit2 = new boolean[N][N];
+        for (int i = 0; i < N; i++) {
+            arr[i] = br.readLine().toCharArray();
+        }
+    }
 }
